7bd243f77799e30c1831f1b415656510049ec61b
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2025-12-21 18:33:55 +0300

    reset project and add task starting point

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  const [counter, setCounter] = useState(0);
+
+  function increase() {
+    setCounter(counter + 1);
+  }
+
+  // 1️⃣ TODO: Create a function named decrease
+  // It should reduce the counter by 1
+
+  return (
+    <View style={styles.container}>
+      <Button title="Increase" onPress={increase} />
+      <Text style={styles.text}>{counter}</Text>
+
+      {/* 2️⃣ Add onPress and connect it to the decrease function */}
+      <Button title="Decrease" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 32,
+    marginVertical: 20,
+  },
+});
