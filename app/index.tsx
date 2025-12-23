6cfaa1933460b6f89e307d75056ec85361405281
imhamoud1-lgtm <imhamoud1@gmail.com>
2025-12-23 16:01:45 +0300

    finidh

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,8 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   const [counter, setCounter] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const [locked, setLocked] = useState(false);
 
   function increase() {
     setCounter(counter + 2);
@@ -16,17 +18,23 @@ export default function Index() {
   function reset(){
     setCounter(0) 
   }
-
-
+function showHideMessege (){
+setIsVisible(!isVisible )
+}
+function lockButtons(){
+  setLocked(!locked);
+}
 
   return (
     <View style={styles.container}>
-      <Button title="Increase" onPress={increase} />
+      <Button title="Increase" onPress={increase} disabled={counter>=10|| locked} />
       <Text style={[styles.text,{color: counter < 5 ? "green" : "red" }]}>{counter}</Text>
-     < Button title="Reset"           onPress={reset}/>
+     < Button title="Reset"           onPress={reset}   disabled={locked}  />
       {/* 2️⃣ Add onPress and connect it to the decrease function */}
-      {counter > 7 && <Text>HEY HEY Too many clicks!</Text>} 
-      <Button title="Decrease"   onPress={dicrease} />
+      {counter > 7 && isVisible &&  <Text>HEY HEY Too many clicks!</Text>} 
+      <Button title={isVisible ?  "Hide" : "Show"} onPress={showHideMessege} />
+      <Button title={locked ? "unlock":"lock"} onPress={lockButtons} />
+      <Button title="Decrease"   onPress={dicrease} disabled={locked} /> 
     </View>
   );
 }
@@ -40,5 +48,6 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 32,
     marginVertical: 20,
+    color:"Red" 
   },
 });
